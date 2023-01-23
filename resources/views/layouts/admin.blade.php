b8eda4fd751681a7ce1a40c5283c59a8545de6f8
Michal Kucharzyk <michalkucharzyk@o2.pl>
2023-01-23 22:54:47 +0100

    Feat: Change title

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -8,7 +8,8 @@
     <link rel="icon" type="image/png" sizes="32x32" href="/assets/images/favicon/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/assets/images/favicon/favicon-16x16.png">
     <link rel="manifest" href="/assets/images/favicon/site.webmanifest">
-    <title>{{ config('app.name', 'Laravel') }}Admin - @yield('title')</title>
+
+    <title>{{ config('app.name', 'Laravel') }} - @yield('title')</title>
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
