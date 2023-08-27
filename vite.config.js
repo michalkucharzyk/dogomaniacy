3c7fc7b5f6737514bb12bd9e1905a05239ae18c6
Michał Kucharzyk <michalkucharzyk@o2.pl>
2023-08-27 22:22:33 +0200

    Add docker file

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,13 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    server: {
+        port: 3000,
+        hmr: {host: 'localhost'},
+        watch: {
+            usePolling:true,
+        }
+    },
     plugins: [
         laravel({
             input: [
